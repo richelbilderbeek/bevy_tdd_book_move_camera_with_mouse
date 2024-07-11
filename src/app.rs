e24236fa81e1e8686b61b41f0405b8909030a0b6
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-07-11 09:09:45 +0200

    Works

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,4 @@
+//use bevy::input::mouse::*;
 use bevy::prelude::*;
 
 #[derive(Component)]
@@ -11,8 +12,9 @@ pub fn create_app() -> App {
     if cfg!(test) {
         app.add_plugins(bevy::input::InputPlugin);
     }
+
     app.add_systems(Startup, (add_camera, add_player));
-    app.add_systems(Update, respond_to_keyboard);
+    app.add_systems(Update, (respond_to_mouse_button_press, respond_to_mouse_move, respond_to_mouse_wheel_turn));
 
     // Do not do update, as this will disallow to do more steps
     // app.update(); //Don't!
@@ -29,13 +31,60 @@ fn add_player(mut commands: Commands) {
     commands.spawn((
         SpriteBundle {
             transform: Transform {
-                scale: Vec3::new(64.0, 32.0, 1.0),
+                scale: Vec3::new(64.0, 32.0, 0.0),
                 ..default()
             },
             ..default()
         },
         Player,
     ));
+}
+
+fn respond_to_mouse_button_press(
+    mut query: Query<&mut Transform, With<Camera>>,
+    input: Res<ButtonInput<MouseButton>>,
+) {
+    let mut transform = query.single_mut();
+    if input.pressed(MouseButton::Left) {
+        transform.rotate_z(0.1);
+    }
+    if input.pressed(MouseButton::Right) {
+        transform.rotate_z(-0.1);
+    }
+}
+
+fn respond_to_mouse_move(
+    mut query: Query<&mut Transform, With<Camera>>,
+    mut mouse_motion_event: EventReader<bevy::input::mouse::MouseMotion>,
+) {
+    for event in mouse_motion_event.read() {
+        let mut transform = query.single_mut();
+        transform.translation.x += event.delta.x / 20.0;
+        transform.translation.y -= event.delta.y / 20.0;
+    }
+}
+
+fn respond_to_mouse_wheel_turn(
+    mut query: Query<&mut OrthographicProjection, With<Camera>>,
+    mut mouse_wheel_event: EventReader<bevy::input::mouse::MouseWheel>,
+) {
+    for event in mouse_wheel_event.read() {
+        let mut projection = query.single_mut();
+        // Do something
+        projection.scale *= 1.0 + ((event.x + event.y) / 10.0);
+    }
+}
+
+#[cfg(test)]
+fn count_n_cameras(app: &mut App) -> usize {
+    let mut query = app.world_mut().query::<&Camera>();
+    query.iter(app.world()).len()
+}
+
+#[cfg(test)]
+fn count_n_players(app: &mut App) -> usize {
+    let mut query = app.world_mut().query::<&Player>();
+    query.iter(app.world()).len()
 }
 
 #[cfg(test)]
@@ -46,27 +95,18 @@ fn get_camera_position(app: &mut App) -> Vec2 {
 }
 
 #[cfg(test)]
-fn get_camera_rotation(app: &mut App) -> f32 {
-    let mut query = app.world_mut().query::<(&Transform, &Camera)>();
-    let (transform, _) = query.single(app.world());
-    transform.rotation.z
-}
-
-#[cfg(test)]
-fn get_camera_zoom(app: &mut App) -> f32 {
-    let mut query = app.world_mut().query::<(&OrthographicProjection, &Camera)>();
-    let (projection, _) = query.single(app.world());
-    projection.scale
-}
-
-
-#[cfg(test)]
 fn get_player_position(app: &mut App) -> Vec2 {
     let mut query = app.world_mut().query::<(&Transform, &Player)>();
     let (transform, _) = query.single(app.world());
     transform.translation.xy()
 }
 
+#[cfg(test)]
+fn get_camera_scale(app: &mut App) -> f32 {
+    let mut query = app.world_mut().query::<(&OrthographicProjection, &Camera)>();
+    let (projection, _) = query.single(app.world());
+    projection.scale
+}
 #[cfg(test)]
 fn get_player_scale(app: &mut App) -> Vec2 {
     let mut query = app.world_mut().query::<(&Transform, &Player)>();
@@ -75,53 +115,16 @@ fn get_player_scale(app: &mut App) -> Vec2 {
 }
 
 #[cfg(test)]
-fn count_n_cameras(app: &mut App) -> usize {
-    let mut query = app.world_mut().query::<&Camera>();
-    query.iter(app.world()).len()
+fn get_camera_rotation(app: &mut App) -> f32 {
+    let mut query = app.world_mut().query::<(&Transform, &Camera)>();
+    let (transform, _) = query.single(app.world());
+    transform.rotation.z
 }
-
-fn respond_to_keyboard(
-    mut query: Query<(&mut Transform, &mut OrthographicProjection, &Camera)>,
-    input: Res<ButtonInput<KeyCode>>,
-) {
-    let (mut transform, mut projection, _) = query.single_mut();
-    use bevy::input::keyboard::KeyCode;
-    if input.pressed(KeyCode::ArrowRight) {
-        transform.translation.x += 1.0;
-    }
-    if input.pressed(KeyCode::ArrowLeft) {
-        transform.translation.x -= 1.0;
-    }
-    if input.pressed(KeyCode::ArrowUp) {
-        transform.translation.y += 1.0;
-    }
-    if input.pressed(KeyCode::ArrowDown) {
-        transform.translation.y -= 1.0;
-    }
-    if input.pressed(KeyCode::KeyQ) {
-        transform.rotate_z(-0.1);
-    }
-    if input.pressed(KeyCode::KeyE) {
-        transform.rotate_z(0.1);
-    }
-    if input.pressed(KeyCode::KeyW) {
-        projection.scale /= 1.1
-    }
-    if input.pressed(KeyCode::KeyS) {
-        projection.scale *= 1.1
-    }
-}
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_empty_app_has_no_cameras() {
-        let mut app = App::new();
-        assert_eq!(count_n_cameras(&mut app), 0);
-    }
+    //use bevy::input::keyboard::KeyCode;
 
     #[test]
     fn test_can_create_app() {
@@ -129,10 +132,16 @@ mod tests {
     }
 
     #[test]
-    fn test_create_app_has_a_moving_camera() {
+    fn test_empty_app_has_no_players() {
+        let mut app = App::new();
+        assert_eq!(count_n_players(&mut app), 0);
+    }
+
+    #[test]
+    fn test_create_app_has_a_player() {
         let mut app = create_app();
         app.update();
-        assert_eq!(count_n_cameras(&mut app), 1);
+        assert_eq!(count_n_players(&mut app), 1);
     }
 
     #[test]
@@ -150,10 +159,24 @@ mod tests {
     }
 
     #[test]
+    fn test_create_app_has_a_camera() {
+        let mut app = create_app();
+        app.update();
+        assert_eq!(count_n_cameras(&mut app), 1);
+    }
+
+    #[test]
     fn test_camera_is_at_origin() {
         let mut app = create_app();
         app.update();
         assert_eq!(get_camera_position(&mut app), Vec2::new(0.0, 0.0));
+    }
+
+    #[test]
+    fn test_camera_is_at_regular_zoom() {
+        let mut app = create_app();
+        app.update();
+        assert_eq!(get_camera_scale(&mut app), 1.0);
     }
 
     #[test]
@@ -164,125 +187,59 @@ mod tests {
     }
 
     #[test]
-    fn test_camera_is_not_zoomed_in_or_out_at_start() {
-        let mut app = create_app();
-        app.update();
-        assert_eq!(get_camera_zoom(&mut app), 1.0);
-    }
-
-    #[test]
-    fn test_camera_moves_when_pressed_up() {
+    fn test_camera_responds_to_mouse_move() {
         let mut app = create_app();
         app.update();
         assert_eq!(get_camera_position(&mut app), Vec2::new(0.0, 0.0));
 
-        // Press the key
-        app.world_mut()
-            .resource_mut::<ButtonInput<KeyCode>>()
-            .press(KeyCode::ArrowUp);
+        // Move the mouse
+        app.world_mut().send_event(bevy::input::mouse::MouseMotion {
+            delta: Vec2::new(100.0, 100.0),
+        });
+
         app.update();
         assert_ne!(get_camera_position(&mut app), Vec2::new(0.0, 0.0));
-
-    }
-    #[test]
-    fn test_camera_moves_when_pressed_right() {
-        let mut app = create_app();
-        app.update();
-        assert_eq!(get_camera_position(&mut app), Vec2::new(0.0, 0.0));
-
-        // Press the key
-        app.world_mut()
-            .resource_mut::<ButtonInput<KeyCode>>()
-            .press(KeyCode::ArrowRight);
-        app.update();
-        assert_ne!(get_camera_position(&mut app), Vec2::new(0.0, 0.0));
-
-    }
-    #[test]
-    fn test_camera_moves_when_pressed_down() {
-        let mut app = create_app();
-        app.update();
-        assert_eq!(get_camera_position(&mut app), Vec2::new(0.0, 0.0));
-
-        // Press the key
-        app.world_mut()
-            .resource_mut::<ButtonInput<KeyCode>>()
-            .press(KeyCode::ArrowDown);
-        app.update();
-        assert_ne!(get_camera_position(&mut app), Vec2::new(0.0, 0.0));
-
-    }
-    #[test]
-    fn test_camera_moves_when_pressed_left() {
-        let mut app = create_app();
-        app.update();
-        assert_eq!(get_camera_position(&mut app), Vec2::new(0.0, 0.0));
-
-        // Press the key
-        app.world_mut()
-            .resource_mut::<ButtonInput<KeyCode>>()
-            .press(KeyCode::ArrowLeft);
-        app.update();assert_ne!(get_camera_position(&mut app), Vec2::new(0.0, 0.0));
-
     }
 
     #[test]
-    fn test_camera_rotates_when_pressed_q() {
+    fn test_camera_responds_to_mouse_button_press() {
         let mut app = create_app();
+        assert!(app.is_plugin_added::<bevy::input::InputPlugin>());
         app.update();
+
+        // Not moved yet
         assert_eq!(get_camera_rotation(&mut app), 0.0);
 
-        // Press the key
+        // Press the left mouse button
         app.world_mut()
-            .resource_mut::<ButtonInput<KeyCode>>()
-            .press(KeyCode::KeyQ);
+            .resource_mut::<ButtonInput<MouseButton>>()
+            .press(MouseButton::Left);
+
         app.update();
 
+        // Position must have changed now
         assert_ne!(get_camera_rotation(&mut app), 0.0);
     }
 
     #[test]
-    fn test_camera_rotates_when_pressed_e() {
+    fn test_camera_responds_to_mouse_wheel_turn() {
         let mut app = create_app();
-        app.update();
-        assert_eq!(get_camera_rotation(&mut app), 0.0);
-
-        // Press the key
-        app.world_mut()
-            .resource_mut::<ButtonInput<KeyCode>>()
-            .press(KeyCode::KeyE);
+        assert!(app.is_plugin_added::<bevy::input::InputPlugin>());
         app.update();
 
-        assert_ne!(get_camera_rotation(&mut app), 0.0);
+        // Not moved yet
+        assert_eq!(get_camera_scale(&mut app), 1.0);
+
+        // Scroll the mouse
+        app.world_mut().send_event(bevy::input::mouse::MouseWheel {
+            unit: bevy::input::mouse::MouseScrollUnit::Line,
+            x: 10.0,
+            y: 10.0,
+            window: Entity::PLACEHOLDER,
+        });
+        app.update();
+
+        // Moved now
+        assert_ne!(get_camera_scale(&mut app), 1.0);
     }
-
-    #[test]
-    fn test_camera_zooms_in_when_pressed_w() {
-        let mut app = create_app();
-        app.update();
-        assert_eq!(get_camera_zoom(&mut app), 1.0);
-
-        // Press the key
-        app.world_mut()
-            .resource_mut::<ButtonInput<KeyCode>>()
-            .press(KeyCode::KeyW);
-        app.update();
-
-        assert!(get_camera_zoom(&mut app) < 1.0);
-    }
-    #[test]
-    fn test_camera_zoom_out_when_pressed_s() {
-        let mut app = create_app();
-        app.update();
-        assert_eq!(get_camera_zoom(&mut app), 1.0);
-
-        // Press the key
-        app.world_mut()
-            .resource_mut::<ButtonInput<KeyCode>>()
-            .press(KeyCode::KeyS);
-        app.update();
-
-        assert!(get_camera_zoom(&mut app) > 1.0);
-    }
-
 }
