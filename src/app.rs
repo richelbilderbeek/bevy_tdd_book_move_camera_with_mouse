7bfea9bbbbc9046b0d31cb62b1307f2c5560aecb
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-12-21 14:24:53 +0100

    Use 'Sprite' instead of -now absent- 'SpriteBundle'

--- src/app.rs
+++ src/app.rs
@@ -39,7 +39,7 @@ fn add_camera(mut commands: Commands) {
 
 fn add_player(mut commands: Commands) {
     commands.spawn((
-        SpriteBundle {
+        Sprite {
             transform: Transform {
                 scale: Vec3::new(64.0, 32.0, 0.0),
                 ..default()
